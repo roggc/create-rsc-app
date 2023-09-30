700892742b837c71b87123586f4c477db81133f9
roger gomez <rgomez@jumpmath.es>
2023-09-30 21:50:22 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# create-rsc-app
+
+To use this package you must do `npx create-rsc-app name-of-the-app`, to create a RSC (React Server Components) app, or, if you want also to have SSR (Server Side Rendering), you must type `npx create-rsc-app name-of-the-app --ssr`.
---
+++ cli.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+
+import { program } from "commander";
+// import packageJson from "./package.json";
+import { exec } from "child_process";
+import path from "path";
+
+// program.version(packageJson.version);
+
+program
+  .arguments("<project-name>")
+  .description("Create a new React Server Components project")
+  .option("--ssr", "create rsc + ssr app")
+  .action((projectName, options) => {
+    const projectPath = path.join(process.cwd(), projectName);
+    let repositoryUrl = "https://github.com/roggc/rsc.git";
+
+    if (options.ssr) {
+      repositoryUrl = "https://github.com/roggc/rsc-ssr.git";
+    }
+
+    console.log(
+      `Creating a new React Server Components project: ${projectName}`
+    );
+
+    // Clone the repository
+    exec(
+      `git clone ${repositoryUrl} ${projectName}`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error cloning the repository: ${error}`);
+          return;
+        }
+
+        // Change to the project directory
+        process.chdir(projectPath);
+
+        // Install project dependencies
+        exec("npm install", (error, stdout, stderr) => {
+          if (error) {
+            console.error(`Error installing dependencies: ${error}`);
+            return;
+          }
+
+          console.log("Project setup complete.");
+        });
+      }
+    );
+  });
+
+program.parse(process.argv);
